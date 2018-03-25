5b68b052155f5cc3e38226d705bfe0e3fc8b1248
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-25 21:56:51 +0000

    made migrations

---
+++ db/migrate/20180325214646_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rooms do |t|
+      t.string :listing_title
+      t.string :occupancy
+      t.string :location
+      t.decimal :cost
+      t.integer :user_id
+    end
+  end
+end
